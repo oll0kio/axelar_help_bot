b581dd729fc7bb3add0ba25dabb1ce12c0b22fc8
zipik <zipik322@gmail.com>
2022-01-17 01:12:16 +0300

    .env

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -8,7 +8,7 @@ from utils.db_api.db_commands import get_user_by_id, add_new_user, count_users
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    user = await add_new_user()
+    await add_new_user()
     text = ("101(Hello), {user}, my name is AxelarHelperBot!\n"
             "101(Привет), {user}, моё имя AxelarHelperBot!\n"
             "\n"
@@ -18,7 +18,7 @@ async def bot_start(message: types.Message):
             "Please choose a language so I can continue the dialogue with you:\n"
             "Пожалуйста, выбери язык, чтобы мы могли продолжать с тобой диалог:\n"
             ).format(
-        user=message.from_user.full_name,count_users=count_users
+        user=message.from_user.full_name, count_users=await count_users()
     )
     languages_markup = InlineKeyboardMarkup(
         inline_keyboard=
